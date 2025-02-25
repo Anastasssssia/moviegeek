bc880a2d4120eecf908a3d4bc5978942ea0df5fb
Anastasssssia <113131540+Anastasssssia@users.noreply.github.com>
2025-02-25 20:03:48 +0300

    Delete moviegeek-master directory

--- moviegeek-master/db-migrate.sh
+++
@@ -1,6 +0,0 @@
-
-docker-compose run web python manage.py makemigrations --noinput
-docker-compose run web python manage.py migrate --run-syncdb
-
-docker-compose run web python populate_moviegeek.py
-docker-compose run web python populate_ratings.py
